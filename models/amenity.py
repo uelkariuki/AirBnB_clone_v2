151fd1a6c77ef01621aa482f5090ebb9fb2fdc2b
uelkariuki <cratzuel@gmail.com>
2023-08-24 23:32:02 +0000

    Fix: Task 10. DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -11,4 +11,4 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship('Place',
-                                   secondary="place_amenity", viewonly=False, overlaps="amenities")
+                                   secondary="place_amenity")
