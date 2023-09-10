683c71665b2ea1801adb64abc5c43eb7910cae52
uelkariuki <cratzuel@gmail.com>
2023-09-10 18:35:09 +0000

    Fix 1: Task 2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -57,13 +57,12 @@ def do_deploy(archive_path):
         # check if the code is running locally or on remote hosts
         run_locally = os.getenv("run_locally", None)
         if run_locally is None:
-            local(f'rm -rf /data/web_static/releases/{archive_name}/')
+            # local(f'rm -rf /data/web_static/releases/{archive_name}/')
             local(f'mkdir -p /data/web_static/releases/{archive_name}')
             local(f'tar -xzf {archive_path} -C\
  /data/web_static/releases/{archive_name}/')
-            local(f'rm -rf /data/web_static/current')
-            local(f'ln -s /data/web_static/releases/{archive_name}/\
- /data/web_static/current')
+            local(f'rm -rfR /data/web_static/current')
+            local(f'ln -s /data/web_static/releases/{archive_name}/ /data/web_static/current')
             os.environ['run_locally'] = "True"
 
         # Upload the archive to the /tmp/ directory of the web server
