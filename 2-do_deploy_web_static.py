94669eebdd3f939607308ef9e1a4c207c2f657d7
uelkariuki <cratzuel@gmail.com>
2023-09-10 17:23:03 +0000

    Task 2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,72 +1,105 @@
 #!/usr/bin/python3
+
 """
-Fabric script (based on the file 1-pack_web_static.py) that
-distributes an archive to your web servers, using the function do_deploy:
+ Fabric script that generates a .tgz archive from the contents of the
+ web_static folder of your AirBnB Clone repo, using the function do_pack.
 """
 
 from fabric.api import run, env, run, put, sudo, local
 import os.path
+from datetime import datetime
 """ Importing the required modules"""
 
-env.hosts = ["34.202.164.69", "100.25.41.114"]
+
+def do_pack():
+    """ Function to help to generate the archive"""
+
+    now = datetime.now()
+    year = now.strftime("%Y")
+    month = now.strftime("%m")
+    day = now.strftime("%d")
+    hour = now.strftime("%H")
+    minute = now.strftime("%M")
+    second = now.strftime("%S")
+
+    local("mkdir -p versions")
+
+    archive_name = f"versions/web_static_{year}{month}{day}{hour}\
+{minute}{second}.tgz"
+    # capture the result
+    result = local(f"tar -czvf {archive_name} web_static", capture=True)
+
+    if result.failed:
+        return None
+    else:
+        return archive_name
+
+
+"""
+Fabric script (based on the file 1-pack_web_static.py) that
+distributes an archive to your web servers, using the function do_deploy:
+"""
+""" Importing the required modules"""
+
+env.hosts = ['34.202.164.69', '100.25.41.114']
 
 
 def do_deploy(archive_path):
-    """ Function to help distributes an archive to the web servers"""
-
-    """# check if the codde is running locally or on remote hosts
-    local_running = os.getenv("runned_locally", None)
-    if local_running is None:
-        # code for running locally (before deployment to the remote hosts)
-
-        if os.path.isfile(archive_path) is False:
-            return False
-        # set the runned_locally environment variable to prevent running
-        # these commands again on remote hosts
-        os.environ["runned_locally"] = "True"""
+    """ Distributes an archive to the web servers"""
 
     if os.path.isfile(archive_path) is False:
         return False
-    # Uncompress the archive to the folder /data/web_static/releases/
-    # <archive filename without extension> on the web server
-    archive_filename = os.path.basename(archive_path)
-    archive_name = os.path.splitext(archive_filename)[0]
+    try:
+        # Uncompress the archive to the folder /data/web_static/releases/
+        # <archive filename without extension> on the web server
+        archive_filename = archive_path.split("/")[-1]
+        archive_name = archive_filename.split(".")[0]
 
-    # Upload the archive to the /tmp/ directory of the web server
+        # check if the code is running locally or on remote hosts
+        run_locally = os.getenv("run_locally", None)
+        if run_locally is not None:
+            l_archive_path = os.path.join('/tmp', archive_filename)
+            l_target_path = os.path.join('/data/web_static/releases',
+                                         archive_name)
+            local(f'rm -rf {l_target_path}')
+            local(f'mkdir -p {l_target_path}')
+            local(f'tar -xzf {l_archive_path} -C {l_target_path}')
+            local(f'rm {l_archive_path}')
 
-    if put(archive_path, f'/tmp/{archive_filename}').failed is True:
-        return False
-    if run(f'rm -rf /data/web_static/releases/{archive_name}/').failed is True:
-        return False
-    if run(f'mkdir -p /data/web_static/releases/{archive_name}/')\
-       .failed is True:
-        return False
-    if run(f'tar -xzf /tmp/{archive_filename} -C\
- /data/web_static/releases/{archive_name}/').failed is True:
-        return False
-    # Delete the archive from the web server
-    if run(f'rm /tmp/{archive_filename}').failed is True:
-        return False
+            local(f'mv {os.path.join(l_target_path, "/web_static/*")}'
+                  f'/data/web_static/releases/{archive_name}/')
 
-    if run(f'mv /data/web_static/releases/{archive_name}/web_static/* '
-           f'/data/web_static/releases/{archive_name}/').failed is True:
-        return False
+            local(f'rm -rf {os.path.join(l_target_path, "/web_static/*")}')
+            local(f'rm -rf /data/web_static/current')
+            local(f'ln -s {l_target_path} /data/web_static/current')
+            os.environ['run_locally'] = "True"
 
-    if run(f'rm -rf /data/web_static/releases/{archive_name}/web_static/* ')\
-       .failed is True:
-        return False
-    # Delete the symbolic link /data/web_static/current from the web server
-    if run(f'rm -rf /data/web_static/current').failed is True:
-        return False
+        # Upload the archive to the /tmp/ directory of the web server
 
-    # Create a new the symbolic link /data/web_static/current on the
-    # web server linked to the new version of your code (/data/web_static/
-    # releases/<archive filename without extension>)
+        put(archive_path, f'/tmp/{archive_filename}')
+        run(f'rm -rf /data/web_static/releases/{archive_name}/')
+        run(f'mkdir -p /data/web_static/releases/{archive_name}/')
+        run(f'tar -xzf /tmp/{archive_filename} -C\
+ /data/web_static/releases/{archive_name}/')
+        # Delete the archive from the web server
+        run(f'rm /tmp/{archive_filename}')
 
-    if run(f'ln -s /data/web_static/releases/{archive_name}/\
- /data/web_static/current').failed is True:
+        run(f'mv /data/web_static/releases/{archive_name}/web_static/* '
+            f'/data/web_static/releases/{archive_name}/')
+
+        run(f'rm -rf /data/web_static/releases/{archive_name}/web_static/*')
+        # Delete the symbolic link /data/web_static/current from the web server
+        run(f'rm -rf /data/web_static/current')
+
+        # Create a new the symbolic link /data/web_static/current on the
+        # web server linked to the new version of your code (/data/web_static/
+        # releases/<archive filename without extension>)
+
+        run(f'ln -s /data/web_static/releases/{archive_name}/\
+ /data/web_static/current')
+
+        # Returns True if all operations have been done correctly,
+        # otherwise returns False
+        return True
+    except Exception:
         return False
-
-    # Returns True if all operations have been done correctly,
-    # otherwise returns False
-    return True
