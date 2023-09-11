8ef41577630ed4b61ba0fbd165da3c333a58a549
uelkariuki <cratzuel@gmail.com>
2023-09-11 00:29:25 +0000

    Task 5. Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,39 +1,75 @@
 # Redo the task #0 but by using Puppet
 
+$nginx_config = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+    add_header X-Served-By ${hostname};
+    server_name uelkariuki.tech;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+    location /data/web_static/current/ {
+        alias /data/web_static/current/;
+    }
+    location /redirect_me {
+         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    error_page 404 /404.html;
+    location /404 {
+        internal;
+    }
+
+}"
 # check if nginx is installed
-exec { 'install nginx':
-  command => '/usr/bin/apt-get update && /usr/bin/apt-get install -y nginx',
-  unless => '/usr/bin/which nginx',
-}
+package { 'nginx':
+  ensure => 'present',
+  provider => 'apt'
+} ->
 
 # create directories
 file { ['/data/', '/data/web_static/', '/data/web_static/releases/', '/data/web_static/shared/', '/data/web_static/releases/test/']:
   ensure => 'directory',
-  owner => 'ubuntu',
-  group => 'ubuntu',
-
-}
+} ->
 
 # create a html file
 file { '/data/web_static/releases/test/index.html':
   ensure => 'present',
   content => "<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\nSchool\n</body>\n</html>"
-}
+} ->
 
 # create symbolic link
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test/',
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-# update nginx config
-exec { 'nginx_config_update':
-  command => "/bin/sed -i '/^\tlocation / i\\n\\tlocation /hbnb_static/ {\\n\\t\\talias /data/web_static/current/;\\n\\t}\\n' /etc/nginx/sites-available/default",
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
 
-# restart nginx service
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
-  subscribe => Exec['nginx_config_update'],
+file { '/var/www/html/index.html':
+  ensure => 'present',
+  content => "Holberton School \n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure => 'present',
+  content =>  $nginx_config
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
