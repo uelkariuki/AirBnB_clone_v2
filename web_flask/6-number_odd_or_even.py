f9a82aefca168e391c6175abc55b28b5ad7d5d70
uelkariuki <cratzuel@gmail.com>
2023-09-22 16:01:30 +0000

    Add a script that starts a Flask web application with added '/number_odd_or_even/<n>' route

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -55,5 +55,11 @@ def number_template_route(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """ Defines the "/number_odd_or_even/<n>" route"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
